37e418620f71f6a8995e090ca7a303d3c61cd08f
mariomurillo-payulatam <mario.murillo@payulatam.com>
2018-03-18 18:44:47 +0000

    The first commit for initial project

---
+++ src/main/java/com/example/journeys/JourneysApplication.java
@@ -0,0 +1,12 @@
+package com.example.journeys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneysApplication.class, args);
+	}
+}
